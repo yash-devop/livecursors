19f0161ff0aad218dc8734708a55e9e89eec2807
yashdevop <yashkamble.dev@gmail.com>
2025-09-07 21:01:40 +0530

    feat: basic join room

--- server/src/socket.ts
+++ server/src/socket.ts
@@ -1,12 +1,24 @@
 import { Server } from "socket.io";
 import { createServer } from "node:http";
 import { app } from ".";
+import { SocketEvents } from "./constants/events";
 
 const server = createServer(app);
 const io = new Server(server);
 
 io.on("connection", (socket) => {
   console.log("socket connected", socket.id);
+
+  // # JOIN ROOM
+
+  socket.on(SocketEvents.JOIN_ROOM.CONNECT, (data) => {
+    const parsedData = JSON.parse(data);
+    console.log("data", parsedData, typeof data);
+    socket.join("common_room");
+    io.to("common_room").emit(SocketEvents.JOIN_ROOM.SUCCESS, {
+      message: `User ${parsedData?.name} joined.`,
+    });
+  });
 });
 
 server.listen(8000, () => {
