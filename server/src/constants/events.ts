fd44904111e2ab42dd58d6ba230c27a133e713c2
yashdevop <yashkamble.dev@gmail.com>
2025-09-07 21:01:08 +0530

    feat: events obj

---
+++ server/src/constants/events.ts
@@ -0,0 +1,10 @@
+export const SocketEvents = {
+  JOIN_ROOM: {
+    CONNECT: "join_room",
+    SUCCESS: "join_room_success",
+  },
+  CURSOR_MOVE: {
+    CONNECT: "cursor_move",
+    SUCCESS: "cursor_move_success",
+  },
+};
