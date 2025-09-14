1f3878a648561aa7f8af4e29e2710deef8ec0e43
yashdevop <yashkamble.dev@gmail.com>
2025-09-14 20:20:22 +0530

    cors config

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,12 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "./socket";
+import { corsConfig } from "./config";
 
 export const app = express();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: ["http://localhost:5173"],
-  })
-);
+app.use(cors(corsConfig));
 
 app.get("/", (req: Request, res: Response) => {
   return res.json({
